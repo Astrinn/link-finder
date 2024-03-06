b115a97a4d80529b3e7b7c11d92ea41c3f0a30fe
Hiago.D <hiagofdss900@gmail.com>
2024-03-05 23:15:13 -0400

    initial commit

---
+++ old/src_old/cli.js
@@ -0,0 +1,33 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import pegaArquivo from './index.js';
+
+const caminho = process.argv;
+
+function imprimeLista(resultado) {
+    console.log(chalk.yellow('lista de links'), resultado)
+};
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    // try{
+    //     fs.lstatSync(caminho);
+    // } catch (erro) {
+    //     if(erro.code === 'ENOENT'){
+    //         console.log('Arquivo ou Diretorio nao existe')
+    //         return;
+    //     }
+    // }
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2]);
+        imprimeLista(resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+            arquivos.forEach(async (nomeDeArquivo) => {
+                const lista = pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+                imprimeLista(lista);
+            })
+    }
+};
